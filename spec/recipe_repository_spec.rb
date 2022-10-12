92d92e9121ce8dfb5840aac92c7bbfafb9cecb8c
Ella Skolnick <ellaskolnick@me.com>
2022-10-12 16:40:33 +0100

    addition to tests

--- spec/recipe_repository_spec.rb
+++ spec/recipe_repository_spec.rb
@@ -2,7 +2,7 @@ require 'recipe_repository'
 
 def reset_recipes_table
   seed_sql = File.read('spec/seeds_recipes.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'recipes_directory' })
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'recipes_directory_test' })
   connection.exec(seed_sql)
 end
 
